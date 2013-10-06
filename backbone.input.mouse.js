08f49b51c0575946fd6321003609aca3be58e3b3
Makis Tracend <makis.tracend@gmail.com>
2013-10-05 18:10:30 -0700

    reforming lib - adding click event

--- backbone.input.mouse.js
+++ backbone.input.mouse.js
@@ -1,51 +1,96 @@
 /*
  * Backbone Mouse
  * - monitor mouse movements in your views
- * 
+ *
  * Created by: Makis Tracend (@tracend)
  * (c) Makesites.org
- * 
+ *
  * Released under the MIT License
- * 
+ *
  */
-(function (factory) {
+ (function (factory) {
 
-    "use strict";
+	"use strict";
 
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['backbone', 'underscore', 'jquery'], factory);
-    } else {
-        // Browser globals
-        factory(Backbone, _, $);
-    }
+	//if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+	//    define(['backbone', 'underscore', 'jquery'], factory);
+	//} else {
+		// Browser globals
+		factory(Backbone, _, $);
+	//}
 }(function (Backbone, _, $) {
 
-    "use strict";
+	"use strict";
 
-    _.extend(Backbone.View.prototype, {
-        state : {
-            hover : false
-        }, 
-        
-        events: {
-            'mouseover': '_mouseover',
-            'mousemove': '_mousemove'
-        },
-    
-        _mouseover: function( e ) {
-            this.state.hover = true;
-        }, 
-        
-        _mousemove: function( e ) {
-            this.mouse = {
-                x : e.pageX, 
-                y : e.pageY
-            };
-            if( this.update ) this.update();
-        }
-    
-    });
+	Backbone.View = Backbone.View.extend({
 
-    return Backbone;
+		state : {
+			hover : false
+		},
+		/* example events:*/
+		events: {
+			'mouseover *' : '_mouseover',
+			'mousemove *' : '_mousemove'
+		},
+
+		monitor: function( state ){
+
+			switch( state ){
+				case "move":
+					this.el.addEventListener( 'mousemove', bind( this, this._mousemove ), false );
+				break;
+				case "click":
+					this.el.addEventListener( 'mousedown', bind( this, this._mousedown ), false );
+					this.el.addEventListener( 'mouseup',   bind( this, this._mouseup ), false );
+				break;
+
+			}
+
+
+		},
+
+		_mousedown: function( e ) {
+			// set position of mouse
+			this.mouse = {
+				x : e.pageX,
+				y : e.pageY
+			};
+			// use clientX instead of pageX?
+			//console.log("mouse pressed");
+			if(this.mousedown) this.mousedown( e );
+		},
+
+		_mouseup: function( e ) {
+			//console.log("mouse released");
+		},
+
+		_mouseover: function( e ) {
+			console.log("mouseover");
+			console.log( e );
+			this.state.hover = true;
+		},
+
+		_mousemove: function( e ) {
+			this.mouse = {
+				x : e.pageX,
+				y : e.pageY
+			};
+		}
+
+	});
+
+// Helpers
+
+function bind( scope, fn ) {
+
+	return function () {
+
+		fn.apply( scope, arguments );
+
+	};
+
+};
+
+	return Backbone;
 }));
